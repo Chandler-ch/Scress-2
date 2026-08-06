56e89537fe654e2ac60eb2d35bb237c472abd2d1
Amelia Chandler <a.chandler@rafisa.ch>
2026-08-06 10:59:16 +0200

    SCII-4: Erster King steht.

---
+++ src/components/ScressFigure.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  figureName: {
+    type: String,
+  },
+})
+console.log(`../assets/${props.figureName}.png`)
+</script>
+<template>
+  <img class="figure" src="../assets/König.png" />
+</template>
+<style scoped>
+.figure {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/ScressTile.vue
+++ src/components/ScressTile.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import ScressFigure from './ScressFigure.vue'
+
 const tileSize = import.meta.env.VITE_TILE_SIZE + 'px'
 </script>
 
 <template>
-  <div class="tile"></div>
+  <div class="tile">
+    <ScressFigure figureName="König" />
+  </div>
 </template>
 
 <style scoped>
