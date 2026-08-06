4963ee12a1c7104b2d0b22729872fc0c337b567e
Amelia Chandler <a.chandler@rafisa.ch>
2026-08-06 16:56:08 +0200

    SCII-10: notes for future implementation

---
+++ src/utils/selectHandling.ts
@@ -0,0 +1,28 @@
+import { selectWPawnPath } from '@/composables/pawnPath'
+
+export function onSelectedFigure(figure: String, currentPosition: Number) {
+  showMovement(figure, currentPosition)
+}
+
+function showMovement(figure: String, currentPosition: Number) {
+  // figure = "Bauer-W"
+  // currentPosition = 50
+  switch (figure) {
+    case 'Bauer':
+    // 2 nach unten
+    case 'Bauer-W':
+      // 2 nach oben
+      selectWPawnPath(currentPosition)
+    case 'Turm':
+    // passt auch für Turm-W
+    // gerade nach in alle 4 Himmelsrichtungen
+    case 'Pferd':
+    //kompliziert
+    case 'Königin':
+    // basically alles ausser Pferd
+    case 'Läufer':
+    // Diagonal überallhin
+    case 'König':
+    // überall 1
+  }
+}
