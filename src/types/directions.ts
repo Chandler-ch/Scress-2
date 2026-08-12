bc2c5e0bd89d0f4fb6e3be13cceb488129a552e8
Chandler-ch <rchandler@mail.ch>
2026-08-12 10:19:58 +0200

    SCII-11: updated edges -> bugfix

--- src/types/directions.ts
+++ src/types/directions.ts
@@ -2,7 +2,7 @@ export type Direction =
   'north' | 'west' | 'south' | 'east' | 'northwest' | 'southwest' | 'northeast' | 'southeast'
 
 export type DirectionMultiplier = { multiplier: number; edge: number[] }
-export const northEdge = [1, 2, 3, 4, 5, 6, 7, 8]
-export const eastEdge = [8, 16, 24, 32, 40, 48, 56, 64]
-export const southEdge = [57, 58, 59, 60, 61, 62, 63, 64]
-export const westEdge = [1, 9, 17, 25, 33, 41, 49, 57]
+export const northEdge = [0, 1, 2, 3, 4, 5, 6, 7]
+export const eastEdge = [7, 15, 23, 31, 39, 47, 55, 63]
+export const southEdge = [56, 57, 58, 59, 60, 61, 62, 63]
+export const westEdge = [0, 8, 16, 24, 32, 40, 48, 56]
