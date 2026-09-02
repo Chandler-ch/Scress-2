f1632e0f118bc07d480f798f5b6f76b1ce835e09
Chandler-ch <rchandler@mail.ch>
2026-09-02 14:11:59 +0200

    SCII-32: typo

--- frontend/src/utils/possibleMovesHandler.ts
+++ frontend/src/utils/possibleMovesHandler.ts
@@ -5,7 +5,7 @@ import { ref, type Ref } from 'vue'
 
 export const possibleMoves: Ref<number[]> = ref([])
 
-export function getpossibleMoves(pos: number, direction: Direction, limited: number = 8) {
+export function getPossibleMoves(pos: number, direction: Direction, limited: number = 8) {
   const multiplier: DirectionMultiplier = resolveDirection(direction)
   checkDirectionTiles(pos, multiplier, limited)
 }
