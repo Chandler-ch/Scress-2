125daf9c21e250d224e96a4020aa5ed4c1ff8363
Chandler-ch <rchandler@mail.ch>
2026-09-10 15:06:17 +0200

    SCII-50: Added new general figurePath file

---
+++ frontend/src/utils/figureSelection/figurePath.ts
@@ -0,0 +1,9 @@
+import type { Direction } from '@/types/directions'
+import { checkDirectionTiles } from '@/utils/checkTilesDirection'
+
+export function selectFigurePath(pos: number, directions: Direction[], limited: number = 8) {
+  directions.forEach((direction) => {
+    checkDirectionTiles(pos, direction, limited)
+    console.log(direction)
+  })
+}
