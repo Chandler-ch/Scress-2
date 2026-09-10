893b42430b5e13d685644dc90560e859c541fe67
Chandler-ch <rchandler@mail.ch>
2026-09-10 15:06:41 +0200

    SCII-50: started adding figureAssignment to figures to make everything easier

---
+++ frontend/src/utils/figureAssignment.ts
@@ -0,0 +1,68 @@
+import type { Direction } from '@/types/directions'
+import type { ScressFigures } from '@/types/scressFigures'
+import { selectFigurePath } from './figureSelection/figurePath'
+import { ref, type Ref } from 'vue'
+import { selectPawnPath } from './figureSelection/pawnPath'
+import { selectQueenPath } from './figureSelection/queenPath'
+import { selectRookPath } from './figureSelection/rookPath'
+
+const directions: Ref<Direction[]> = ref([])
+const limited: Ref<number> = ref(8)
+
+export function showMovement(pos: number, currentFigure: ScressFigures) {
+  resetValues()
+
+  switch (currentFigure) {
+    case 'Bauer':
+    case 'Bauer-W':
+      selectPawnPath(pos)
+      break
+    case 'König':
+    case 'König-W':
+      directions.value = [
+        'north',
+        'east',
+        'south',
+        'west',
+        'northeast',
+        'northwest',
+        'southwest',
+        'southeast',
+      ]
+      limited.value = 1
+      break
+    case 'Königin':
+    case 'Königin-W':
+      selectQueenPath(pos)
+      break
+    case 'Läufer':
+    case 'Läufer-W':
+      directions.value = ['northeast', 'northwest', 'southeast', 'southwest']
+      break
+    case 'Pferd':
+    case 'Pferd-W':
+      limited.value = 1
+      directions.value = [
+        'kNorthWest',
+        'kEastNorth',
+        'kEastSouth',
+        'kNorthEast',
+        'kSouthEast',
+        'kSouthWest',
+        'kWestSouth',
+        'kWestNorth',
+      ]
+      break
+    case 'Turm':
+    case 'Turm-W':
+      selectRookPath(pos)
+      break
+  }
+  selectFigurePath(pos, directions.value, limited.value)
+}
+
+function resetValues() {
+  limited.value = 8
+  directions.value = []
+  // hunger
+}
--- frontend/src/utils/figureSelection/bishopPath.ts
+++
@@ -1,9 +0,0 @@
-import type { Direction } from '@/types/directions'
-import { checkDirectionTiles } from '@/utils/checkTilesDirection'
-
-export function selectBishopPath(pos: number) {
-  const directions: Direction[] = ['northeast', 'northwest', 'southeast', 'southwest']
-  directions.forEach((direction) => {
-    checkDirectionTiles(pos, direction)
-  })
-}
